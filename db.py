6c3bf8b5641fab2034bd0343ef0a8cc7353ea68a
Mamta-Kumari-creator <23f2001943@ds.study.iitm.ac.in>
2025-12-25 19:34:41 +0530

    established connection to database

---
+++ db.py
@@ -0,0 +1,15 @@
+import psycopg2
+db_name= "TaskListDB"
+db_user = "tasklistuser"
+db_pw= "Mamta@123"
+db_host= "localhost"
+
+def getTaskList():
+    con=psycopg2.connect(database=db_name,user=db_user,password=db_pw,host=db_host)
+    cur=con.cursor()
+    cur.execute(' select task_name, is_active from public."TaskList"')
+    tasklist=cur.fetchall()
+    con.close()
+    con.close()
+    return tasklist
+
