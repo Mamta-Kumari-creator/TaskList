ebd541a9811d3abaffc5f11c0ac55f929cab3f02
Mamta-Kumari-creator <23f2001943@ds.study.iitm.ac.in>
2025-12-25 22:11:09 +0530

    this little project is completed

--- db.py
+++ db.py
@@ -7,15 +7,24 @@ db_host= "localhost"
 def getTaskList():
     con=psycopg2.connect(database=db_name,user=db_user,password=db_pw,host=db_host)
     cur=con.cursor()
-    cur.execute(' select task_name, is_active from public."TaskList"')
+    cur.execute(' select id,task_name, is_active from public."TaskList"')
     tasklist=cur.fetchall()
     con.commit()
     con.close()
     return tasklist
 
 def addtask(taskname, due_date):
+    executequery(' insert into public."TaskList"(task_name, due_date) values (%s, %s)', (taskname, due_date))
+
+def updatetask(name,id):
+    executequery(' update public."TaskList" set task_name=%s where id=%s', (name,id))
+
+def deletetask(id):
+    executequery(' delete from public."TaskList" where id=%s', (id))
+
+def executequery(query,params=None):
     con=psycopg2.connect(database=db_name,user=db_user,password=db_pw,host=db_host)
     cur=con.cursor()
-    cur.execute(' insert into public."TaskList"(task_name, due_date) values (%s, %s)', (taskname, due_date))
+    cur.execute(query,params)
     con.commit()
     con.close()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from db import addtask, getTaskList
+from db import addtask, deletetask, getTaskList, updatetask
 app = Flask(__name__)
 
 
@@ -14,6 +14,17 @@ def add():
     taskname=request.form['task_name']
     due_date=request.form['due_date']
     addtask(taskname, due_date)
+    return redirect(url_for('home'))
+
+@app.route("/update" , methods=["POST"])
+def update():
+    updatedtaskname=request.form['updateTask']
+    id=request.form['id']
+    button=request.form["save_or_delete"]
+    if button=="save":
+        updatetask(updatedtaskname, id)
+    elif button=="delete":
+        deletetask(id)
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
